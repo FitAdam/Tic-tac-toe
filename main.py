2dea03932cfeb168e24b83b1ee71fc1be67a3e59
FitAdam <adam.george617@gmail.com>
2020-07-25 13:32:18 +0100

    board change to list

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # the board
-the_board = {
-    'a_line' : [[], [], []],
-    'b_line' : [[], [], []],
-    'c_line' : [[], [], []]
-    }
+# class object or list or dict???????? how...
+
+a_line = [[], [], []]
+b_line = [[], [], []]
+c_line = [[], [], []]
+
+the_board = [[], [], [],
+            [], [], [],
+            [], [], []]
 
 # player signs
 
@@ -13,54 +17,51 @@ player2 = 'y'
 # checks
 
 def check_horizontally(new_board):
-    for key, value in new_board.items():
-        if value[0] == value[1] == value[2]:
-            print(f'{value[0]} is the winner.')
-        else:
-            print(f'next move.')
-"""
-        if b_line[0] == b_line[1] == b_line[2]:
-            print(f'{b_line[0]} is the winner.')
-        else:
-            print(f'next move.')
+    if new_board[0] == new_board[1] == new_board[2]:
+        print(f'{new_board[0]} is the winner.')
+    else:
+        print(f'next move.')
 
-        if c_line[0] == c_line[1] == c_line[2]:
-            print(f'{a_line[0]} is the winner.')
-        else:
-            print(f'next move.')
+    if new_board[3] == new_board[4] == new_board[5]:
+        print(f'{new_board[3]} is the winner.')
+    else:
+        print(f'next move.')
+
+    if new_board[6] == new_board[7] == new_board[8]:
+        print(f'{new_board[6]} is the winner.')
+    else:
+        print(f'next move.')
 
 def check_vertically(new_board):
-    if a_line[0] == b_line[0] == c_line[0]:
-        print(f'{a_line[0]} is the winner.')
+    if new_board[0] == new_board[3] == new_board[6]:
+        print(f'{new_board[0]} is the winner.')
     else:
         print(f'next move.')
 
-    if a_line[1] == b_line[1] == c_line[1]:
-        print(f'{b_line[1]} is the winner.')
+    if new_board[1] == new_board[4] == new_board[7]:
+        print(f'{new_board[1]} is the winner.')
     else:
         print(f'next move.')
 
-    if a_line[2] == b_line[2] == c_line[2]:
-        print(f'{a_line[0]} is the winner.')
+    if new_board[2] == new_board[5] == new_board[8]:
+        print(f'{new_board[2]} is the winner.')
     else:
         print(f'next move.')
 
 def check_obliquely(new_board):
-    if a_line[0] == b_line[1] == c_line[2]:
-        print(f'{a_line[0]} is the winner.')
+    if new_board[0] == new_board[4] == new_board[8]:
+        print(f'{new_board[0]} is the winner.')
     else:
         print(f'next move.')
 
-    if a_line[2] == b_line[1] == c_line[0]:
-        print(f'{b_line[1]} is the winner.')
+    if new_board[6] == new_board[4] == new_board[2]:
+        print(f'{new_board[6]} is the winner.')
     else:
         print(f'next move.')
-"""
 
-new_board = {
-    'a_line' : [['x'], ['x'], ['x']],
-    'b_line' : [[], [], []],
-    'c_line' : [[], [], []]
-    }
 
-check_horizontally(new_board)
+the_board = [['x'], ['x'], ['x'],
+            [], [], [],
+            [], [], []]
+
+check_horizontally(the_board)
