0e3229c2b4381e06a7b05f7a436b98e997d019e5
FitAdam <adam.george617@gmail.com>
2020-07-29 13:00:10 +0100

    new improvements

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 # the board
-# class object or list or dict???????? how...
-
-a_line = [[], [], []]
-b_line = [[], [], []]
-c_line = [[], [], []]
 
 the_board = ['1', '2', '3',
             '4', '5', '6',
@@ -11,13 +6,16 @@ the_board = ['1', '2', '3',
 
 # player signs
 
-player1 = 'x'
-player2 = 'y'
+player1 = 'X'
+player2 = 'O'
 
 # checks
 def game_check(new_board):
+
+    """This funcion checks the board if the signs create a line it returns a gameON as False."""
+
     gameOn = True
-    while gameOn:
+    while True:
     # check_horizontally(new_board):
         if new_board[0] == new_board[1] == new_board[2]:
             print(f'{new_board[0]} is the winner.')
@@ -64,13 +62,17 @@ def game_check(new_board):
         else:
             print('Time for the next move.')
             break
-
+    
+    #print(f'The gameOn is {gameOn}')
     return gameOn
 
 
 
 
 def view_board(new_board):
+
+    """This function is used to show the board to the players """
+
     print(f'\n\t  {new_board[0][0]} | {new_board[1][0]} | {new_board[2][0]}')
     print('\t-------------')
     print(f'\t  {new_board[3][0]} | {new_board[4][0]} | {new_board[5][0]}')
@@ -79,32 +81,56 @@ def view_board(new_board):
     print('\t')
 
 
-def user_move(new_board):
+def user_move(new_board, player_sign):
+
+    """This function is used to get user and return it"""
+
     move = []
     message = "Time to move. Choose position from 1 to 9:"
     message += "\nLike this: 5"
-    message += "\nPlayer moves: "
-    field = int(input(message))
-    symbol = str(input('Type your symbol: '))
-    field -= 1
-    move.append(field)
-    move.append(symbol)
-    print(move)
-    return move
+    message += f"\nPlayer {player_sign} moves: "
+    while True:
+        try:
+            field = int(input(message))
+            #symbol = str(input('Type your symbol: '))
+        except ValueError:
+            print("\nOops!  That was no valid number.  Try again...\n")
+            continue
+        else:
+            field -= 1
+            move.append(field)
+            move.append(player_sign)
+            print(move)
+            return move
 
 def allocate_choice(board, move):
+
+    """Update and return the board with allocated move."""
+
     position = board.pop(move[0])
     board.insert(move[0], move[1])
     return board
 
 def main(the_board):
-    gameOn = True
-    while gameOn:
+
+    """Main flow of the game. The gameOn set up to False ends game."""
+    
+    while True:
         view_board(the_board)
-        choice = user_move(the_board)
+        choice = user_move(the_board, player1)
         the_board = allocate_choice(the_board, choice)
         view_board(the_board)
         gameOn = game_check(the_board)
+        if gameOn == False:
+            break
+        #print(f'The gameOn in MAIN is {gameOn}')
+        view_board(the_board)
+        choice = user_move(the_board, player2)
+        the_board = allocate_choice(the_board, choice)
+        view_board(the_board)
+        gameOn = game_check(the_board)
+        if gameOn == False:
+            break
 
 
 main(the_board)
